dd5c10c5cb4547197c952764447e5124b08463de
sindriaBR <barri043006@gmail.com>
2024-01-16 17:42:45 +0100

    Pantalla Inicial Provisional + Creacion Actividades Iniciales (para trabajarlas)

---
+++ app/src/main/java/android/ejemplo/sharewheels/ClientMenuActivity.java
@@ -0,0 +1,14 @@
+package android.ejemplo.sharewheels;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ClientMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_client_menu);
+    }
+}
--- app/src/main/java/android/ejemplo/sharewheels/MainActivity.java
+++ app/src/main/java/android/ejemplo/sharewheels/MainActivity.java
@@ -2,13 +2,35 @@ package android.ejemplo.sharewheels;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button login,register;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        login = findViewById(R.id.buttonLoginPage);
+        register = findViewById(R.id.buttonRegisterPage);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent  = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent  = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
