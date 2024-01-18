f12ea150f820e8e3767c80fa36ec8b3002e26f22
jblores <jbartralores@gmail.com>
2024-01-19 00:17:53 +0100

    Mapa 0.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,6 +39,11 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation(platform("com.google.firebase:firebase-bom:32.7.0"))
     implementation("com.google.firebase:firebase-analytics")
+
+    //Servicios de Google
+    implementation("com.google.android.gms:play-services-maps:18.2.0")
+    implementation("com.google.android.gms:play-services-location:21.0.1")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
