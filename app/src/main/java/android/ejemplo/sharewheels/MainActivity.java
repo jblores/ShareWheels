d49bec009dd970de8b506463b53e7247cc450839
jblores <jbartralores@gmail.com>
2024-01-19 12:52:43 +0100

    Mapa 0.0.3 Arreglado el unas lineas mal ordenadas en el manifest Añadido comprobación de los permisos del mapa Añadido metodo para crear una ventana y habilitar los permisos necesarios

--- app/src/main/java/android/ejemplo/sharewheels/MainActivity.java
+++ app/src/main/java/android/ejemplo/sharewheels/MainActivity.java
@@ -1,13 +1,22 @@
 package android.ejemplo.sharewheels;
 
 import static android.ejemplo.sharewheels.util.Constants.ERROR_DIALOG_REQUEST;
+import static android.ejemplo.sharewheels.util.Constants.PERMISSIONS_REQUEST_ENABLE_GPS;
 
 
+import androidx.activity.result.contract.ActivityResultContract;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -23,7 +32,29 @@ public class MainActivity extends AppCompatActivity {
     private Button login,register;
     private boolean mLocationPermisionGranted = false;
 
+    private void buildAlertMessageNoGps(){
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Esta aplicaci'on requiere de GPS para funcionar.")
+                .setCancelable(false)
+                .setPositiveButton("Si", new DialogInterface.OnClickListener() {
 
+                    public void onClick(@SuppressWarnings("unused")final DialogInterface dialog,@SuppressWarnings("unused") final int id) {
+                        Intent enableGpsIntent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                        //revisar esto que no se si esta bien porque en el video utilizan un metodo diferente y el sustituto no obsoleto de ese metodo es una locura
+                        startActivity(enableGpsIntent);
+                    }
+                });
+        final AlertDialog alert = builder.create();
+        alert.show();
+    }
+    public boolean isMapsEnabled(){
+        final LocationManager manager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (!manager.isProviderEnabled(LocationManager.GPS_PROVIDER)){
+            buildAlertMessageNoGps();
+            return false;
+        }
+        return true;
+    }
     public boolean isServicesOK(){
         Log.d(TAG,"isServicesOK : checking google services version");
         int disponible = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(MainActivity.this);
