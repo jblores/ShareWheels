da39cac272fbb81da7dd5884c160ed91340d3760
jblores <jbartralores@gmail.com>
2024-01-19 12:10:13 +0100

    Mapa 0.0.2 Creada una nueva clase de constantes Añadido al manifest los permisos Añadida la api key

--- app/src/main/java/android/ejemplo/sharewheels/MainActivity.java
+++ app/src/main/java/android/ejemplo/sharewheels/MainActivity.java
@@ -1,15 +1,45 @@
 package android.ejemplo.sharewheels;
 
+import static android.ejemplo.sharewheels.util.Constants.ERROR_DIALOG_REQUEST;
+
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.api.GoogleApi;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button login,register;
+    private static final String TAG = "MainActivity";
+    private Button login,register;
+    private boolean mLocationPermisionGranted = false;
+
+
+    public boolean isServicesOK(){
+        Log.d(TAG,"isServicesOK : checking google services version");
+        int disponible = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(MainActivity.this);
+        if (disponible == ConnectionResult.SUCCESS){
+            Log.d(TAG,"isServicesOK: Google Play Services funciona");
+            return true;
+        }else if (GoogleApiAvailability.getInstance().isUserResolvableError(disponible)){
+            Log.d(TAG,"isServicesOK: ha ocurrido un error pero se puede arreglar");
+            Dialog dialog = GoogleApiAvailability.getInstance().getErrorDialog(MainActivity.this,disponible,ERROR_DIALOG_REQUEST);
+            dialog.show();
+        }else {
+            Toast.makeText(this,"No puedes utilizar el mapa",Toast.LENGTH_LONG).show();
+        }
+        return false;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
---
+++ app/src/main/java/android/ejemplo/sharewheels/util/Constants.java
@@ -0,0 +1,8 @@
+package android.ejemplo.sharewheels.util;
+
+public class Constants {
+    public static final int ERROR_DIALOG_REQUEST = 9001;
+    public static final int PERMISSIONS_REQUEST_ENABLE_GPS = 9002;
+    public static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 9003;
+    public static final int PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION = 9004;
+}
